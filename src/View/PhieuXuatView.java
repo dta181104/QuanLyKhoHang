583211d5c56b5858c7b83cd2a6e2ab3bdd471f6f
dta181104 <165494716+dta181104@users.noreply.github.com>
2025-07-09 11:51:46 +0700

    Initial commit

--- src/View/PhieuXuatView.java
+++ src/View/PhieuXuatView.java
@@ -14,7 +14,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
-import javax.swing.table.DefaultTableModel;
+// import javax.swing.table.DefaultTableModel;
 
 public class PhieuXuatView {
     private JFrame frame;
